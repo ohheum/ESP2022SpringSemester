b56fa5ad30b0736532e1f6d6b1baf4eef2428d8b
ohheum <ohheum@gmail.com>
2022-04-02 12:09:13 +0900

    Add files via upload

---
+++ face_recog_from_video.py
@@ -0,0 +1,96 @@
+# MIT License
+# Copyright (c) 2019-2022 JetsonHacks
+
+# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
+# NVIDIA Jetson Nano Developer Kit using OpenCV
+# Drivers for the camera and OpenCV are included in the base image
+
+import face_recognition
+import cv2
+
+
+"""
+gstreamer_pipeline returns a GStreamer pipeline for capturing from the CSI camera
+Flip the image by setting the flip_method (most common values: 0 and 2)
+display_width and display_height determine the size of each camera pane in the window on the screen
+Default 1920x1080 displayd in a 1/4 size window
+"""
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=640,
+    capture_height=360,
+    display_width=640,
+    display_height=360,
+    framerate=20,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d !"
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "videobalance contrast=1.0 brightness=+0.1 ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+def show_camera():
+    window_title = "CSI Camera"
+
+    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
+    print(gstreamer_pipeline(flip_method=0))
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        try:
+            window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
+            while True:
+                ret_val, frame = video_capture.read()
+                # print(type(frame))
+
+
+                # Resize frame of video to 1/2 size for faster face recognition processing
+                small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+                # print(small_frame.shape)
+
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                rgb_small_frame = small_frame[:, :, ::-1]
+
+
+                face_locations = face_recognition.face_locations(rgb_small_frame)
+                # face_locations = face_recognition.face_locations(small_frame, model="cnn")
+
+                for top, right, bottom, left in face_locations:
+                    print(top, right, bottom, left)
+                    cv2.rectangle(frame, (left*2, top*2), (right*2, bottom*2), (255, 0, 0), 2)
+
+                # Check to see if the user closed the window
+                # Under GTK+ (Jetson Default), WND_PROP_VISIBLE does not work correctly. Under Qt it does
+                # GTK - Substitute WND_PROP_AUTOSIZE to detect if window has been closed by user
+                if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
+                    cv2.imshow(window_title, frame)
+                else:
+                    break
+                keyCode = cv2.waitKey(10) & 0xFF
+                # Stop the program on the ESC key or 'q'
+                if keyCode == 27 or keyCode == ord('q'):
+                    break
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Error: Unable to open camera")
+
+
+if __name__ == "__main__":
+    show_camera()
